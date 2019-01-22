4b20ee77bc23b7825d541f66b963e6fd00a4f497
Missra <39300516+missra-kit@users.noreply.github.com>
2019-01-22 17:31:21 +0800

    Create index.jsx

---
+++ react-Placeholder/placeholders/index.jsx
@@ -0,0 +1,19 @@
+import TextRow from './TextRow';
+import RoundShape from './RoundShape';
+import RectShape from './RectShape';
+import TextBlock from './TextBlock';
+import MediaBlock from './MediaBlock';
+
+export {
+  TextRow,
+  RoundShape,
+  RectShape,
+  TextBlock,
+  MediaBlock
+}
+
+export const textRow = TextRow;
+export const round = RoundShape;
+export const rect = RectShape;
+export const text = TextBlock;
+export const media = MediaBlock;
