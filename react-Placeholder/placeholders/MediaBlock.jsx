c7d61d9b66a285f529c7a057afcb8a886232680f
Missra <39300516+missra-kit@users.noreply.github.com>
2019-01-22 17:31:42 +0800

    Create MediaBlock.jsx

---
+++ react-Placeholder/placeholders/MediaBlock.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TextBlock from './TextBlock';
+import RoundShape from './RoundShape';
+
+export default class MediaBlock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { className, style, color, rows } = this.props;
+
+    const classes = ['media-block', className].filter(c => c).join(' ');
+
+    return (
+      <div className={classes} style={{ ...style, display: 'flex' }}>
+        <RoundShape
+          color={color}
+          style={{ minHeight: 55, width: 55, minWidth: 55, marginRight: 10 }}
+        />
+        <TextBlock color={color} rows={rows} />
+      </div>
+    );
+  }
+
+}
+
+MediaBlock.propTypes = {
+  rows: PropTypes.number.isRequired,
+  color: PropTypes.string.isRequired,
+  style: PropTypes.object,
+  className: PropTypes.string
+}
