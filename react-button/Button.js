9e1596c56d5671b0fae147e540ff7f5f89204bbd
Kothing <39289309+kothing@users.noreply.github.com>
2019-07-22 12:03:38 +0800

    Create Button.js

---
+++ react-button/Button.js
@@ -0,0 +1,46 @@
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import './Button.css';
+
+import Ripple from '../Ripple';
+
+class Button extends Component {
+  render() {
+    const { type, size, children, onClick } = this.props;
+    const cls = classnames({
+      'x-button': true,
+      'x-button-default': type === 'default',
+      [`x-button-${type}`]: type,
+      [`x-button-size-${size}`]: size,
+      'x-transition-base': true,
+    });
+    return (
+      <button
+        type="button"
+        className={cls}
+        onClick={onClick}
+      >
+        {children}
+        <Ripple overHidden centerMode={false} rippleColor="#f44336" size="default" />
+      </button>
+    );
+  }
+}
+
+Button.defaultProps = {
+  type: 'default',
+  size: 'default',
+  children: 'Button',
+  onClick: () => {}
+};
+
+Button.propTypes = {
+  type: PropTypes.oneOf(['default', 'primary', 'outline']),
+  size: PropTypes.oneOf(['large', 'default', 'small']),
+  children: PropTypes.string,
+  onClick: PropTypes.func
+};
+
+export default Button;
