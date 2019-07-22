8c20deaa077b516c246dc350e74cf94b55a31a63
Kothing <39289309+kothing@users.noreply.github.com>
2019-07-22 17:41:31 +0800

    Update Trees.jsx

--- react-treeTransfer/Tree-Transfer2/Trees.jsx
+++ react-treeTransfer/Tree-Transfer2/Trees.jsx
@@ -43,6 +43,7 @@ class Trees extends React.Component {
 			expandedKeys = this.props.arrayData
 				.map((item) => {
 					const titleText = `${item.title}`;
+					console.log(titleText.indexOf(value) > -1);
 					if (titleText.indexOf(value) > -1) {
 						return getParentKey(item.key, this.props.arrayData);
 					}
@@ -91,16 +92,18 @@ class Trees extends React.Component {
 						<Search placeholder={placeholder} onChange={this.onChange.bind(this)} />
 					</div> : null
 				}
-				<Tree
-					checkable={true}
-					onExpand={this.onExpand.bind(this)}
-					onCheck={onCheck}
-					checkedKeys={checkedKeys}
-					expandedKeys={expandedKeys}
-					autoExpandParent={autoExpandParent}
-				>
-					{loop(this.props.treeData)}
-				</Tree>
+				<div className="tree-list" style={this.props.style}>
+					<Tree
+						checkable={true}
+						onExpand={this.onExpand.bind(this)}
+						onCheck={onCheck}
+						checkedKeys={checkedKeys}
+						expandedKeys={expandedKeys}
+						autoExpandParent={autoExpandParent}
+					>
+						{loop(this.props.treeData)}
+					</Tree>
+				</div>
 			</div>
 		);
 	}
