aed0081646136de191878ab13ae607398f3142fd
Kothing <39289309+kothing@users.noreply.github.com>
2019-08-08 11:43:38 +0800

    Update Transition.jsx

--- react-transition/Transition.jsx
+++ react-transition/Transition.jsx
@@ -1,7 +1,7 @@
+import React, { cloneElement, Component } from 'react';
+import PropTypes from 'prop-types';
 import cx from 'classnames';
 import _ from 'lodash';
-import PropTypes from 'prop-types';
-import { cloneElement, Component } from 'react';
 import { ALL_TRANSITIONS, DIRECTIONAL_TRANSITIONS } from './consts';
 import TransitionGroup from './TransitionGroup';
 import './transition.css';
