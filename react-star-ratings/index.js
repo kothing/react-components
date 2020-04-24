b5df1aa5fde08dd21c8f2d563523243bc9e3b11f
Kothing <39289309+kothing@users.noreply.github.com>
2020-04-24 10:23:43 +0800

    Create index.js

---
+++ react-star-ratings/index.js
@@ -0,0 +1,8 @@
+// polyfill for ie
+Number.isInteger = Number.isInteger || function(value) {
+  return typeof value === "number" &&  isFinite(value) && Math.floor(value) === value;
+};
+
+import StarRatings from "./StarRatings";
+
+export default StarRatings;
