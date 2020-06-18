9b68f9a7efd359ea7e02d2d98fb67f537ba17df8
Kothing <39289309+kothing@users.noreply.github.com>
2020-06-18 12:00:06 +0800

    Update index.tsx

--- react-captcha/index.tsx
+++ react-captcha/index.tsx
@@ -14,6 +14,7 @@
 
 import React, { useRef, useState, useEffect, useCallback, CSSProperties, MutableRefObject } from 'react';
 import classnames from 'classnames';
+import './style.css';
 
 type LowerLetter = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z';
 type UpperLetter = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z';
