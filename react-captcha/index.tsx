63a33ea635c3ea756de42b84a153bcba9905a9e9
Kothing <39289309+kothing@users.noreply.github.com>
2020-06-26 01:26:47 +0800

    Update index.tsx

--- react-captcha/index.tsx
+++ react-captcha/index.tsx
@@ -90,6 +90,8 @@ interface CaptchaProps {
   className?: string;
   // code change callback
   style?: CSSProperties;
+  // input placeholder
+  placeholder?: string;
   // canvas ref
   ref?: MutableRefObject<HTMLDivElement | null>;
   // code change callback
@@ -208,9 +210,22 @@ const drawTxt = (
   return captchaTxt;
 };
 
+// custom hook
+const useRefCallback = (fn: () => void, dependencies: any) => {
+  const ref = useRef(fn);
+  useEffect(() => {
+    ref.current = fn;
+  }, [fn, ...dependencies]);
+
+  return useCallback(() => {
+    const fn = ref.current;
+    return fn();
+  }, [ref]);
+};
+
 // default props
 const defaultProps = {
-  width: 100,
+  width: 150,
   height: 30,
   type: 'blend',
   length: 4,
@@ -220,6 +235,7 @@ const defaultProps = {
   numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
   className: '',
   style: { cursor: 'pointer' },
+  placeholder: 'Insert captcha',
 };
 
 /**
@@ -234,6 +250,7 @@ const Captcha: React.FC<CaptchaProps> = ({
   numbers = defaultProps.numbers,
   className = defaultProps.className,
   style = defaultProps.style,
+  placeholder = defaultProps.placeholder,
   onChange,
   onInputChange,
 }: CaptchaProps) => {
@@ -244,12 +261,12 @@ const Captcha: React.FC<CaptchaProps> = ({
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   // start draw
-  const draw = useCallback(() => {
+  const draw = useRefCallback(() => {
     if (canvasRef.current) {
       const ctx = canvasRef.current.getContext('2d');
       if (ctx) {
         ctx.textBaseline = 'middle';
-        ctx.fillStyle = randomColor(220, 255);
+        ctx.fillStyle = randomColor(255, 255);
         ctx.fillRect(0, 0, width, height);
         // switch type
         const txtArr = switchTypeAndSetStringToArray(type, numbers, letters);
@@ -282,7 +299,7 @@ const Captcha: React.FC<CaptchaProps> = ({
     } else {
       warning('Can not use canvas');
     }
-  }, []);
+  }, [width, height, type, numbers, letters, length, inputValue, onChange]);
 
   useEffect(() => {
     if (inputRef.current) {
@@ -293,6 +310,18 @@ const Captcha: React.FC<CaptchaProps> = ({
   useEffect(() => {
     draw();
   }, [draw]);
+
+  // Refresh
+  const handleRefresh = () => {
+    draw();
+  };
+
+  // PlayAudio
+  const handlePlayAudio = () => {
+    const audio = new SpeechSynthesisUtterance(captchaValue.toString().split('').join(' '));
+    audio.rate = 0.6;
+    window.speechSynthesis.speak(audio);
+  };
 
   const inputOnChange = (e: any) => {
     if (e.target.value.length <= length) {
@@ -326,22 +355,57 @@ const Captcha: React.FC<CaptchaProps> = ({
 
   return (
     <div className={classnames('react-captcha', className)}>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => inputOnChange(e)}
-        ref={inputRef}
-        className="captcha-input captcha-ui-input"
-      />
-      {validTips()}
-      <canvas
-        ref={canvasRef}
-        onClick={() => draw()}
-        style={style}
-        height={height}
-        width={width}
-        className="captcha-canvas"
-      />
+      <div className="captcha-row">
+        <canvas
+          ref={canvasRef}
+          onClick={() => draw()}
+          style={style}
+          height={height}
+          width={width}
+          className="captcha-canvas"
+        />
+        <div className="captcha-column">
+          <button type="button" onClick={handleRefresh} className="captcha-button">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <g data-name="Layer 2">
+                <g data-name="refresh">
+                  <rect width="24" height="24" opacity="0" />
+                  <path d="M20.3 13.43a1 1 0 0 0-1.25.65A7.14 7.14 0 0 1 12.18 19 7.1 7.1 0 0 1 5 12a7.1 7.1 0 0 1 7.18-7 7.26 7.26 0 0 1 4.65 1.67l-2.17-.36a1 1 0 0 0-1.15.83 1 1 0 0 0 .83 1.15l4.24.7h.17a1 1 0 0 0 .34-.06.33.33 0 0 0 .1-.06.78.78 0 0 0 .2-.11l.09-.11c0-.05.09-.09.13-.15s0-.1.05-.14a1.34 1.34 0 0 0 .07-.18l.75-4a1 1 0 0 0-2-.38l-.27 1.45A9.21 9.21 0 0 0 12.18 3 9.1 9.1 0 0 0 3 12a9.1 9.1 0 0 0 9.18 9A9.12 9.12 0 0 0 21 14.68a1 1 0 0 0-.7-1.25z" />
+                </g>
+              </g>
+            </svg>
+          </button>
+          <button
+            type="button"
+            aria-label="play audio"
+            onClick={handlePlayAudio}
+            className="captcha-button"
+            data-testid="captcha-audio"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <g data-name="Layer 2">
+                <g data-name="volume-up">
+                  <rect width="24" height="24" opacity="0" />
+                  <path d="M18.28 8.37a1 1 0 1 0-1.56 1.26 4 4 0 0 1 0 4.74A1 1 0 0 0 17.5 16a1 1 0 0 0 .78-.37 6 6 0 0 0 0-7.26z" />
+                  <path d="M19.64 5.23a1 1 0 1 0-1.28 1.54A6.8 6.8 0 0 1 21 12a6.8 6.8 0 0 1-2.64 5.23 1 1 0 0 0-.13 1.41A1 1 0 0 0 19 19a1 1 0 0 0 .64-.23A8.75 8.75 0 0 0 23 12a8.75 8.75 0 0 0-3.36-6.77z" />
+                  <path d="M15 3.12a1 1 0 0 0-1 0L7.52 7.57h-5a1 1 0 0 0-1 1v6.86a1 1 0 0 0 1 1h5l6.41 4.4a1.06 1.06 0 0 0 .57.17 1 1 0 0 0 1-1V4a1 1 0 0 0-.5-.88zm-1.47 15L8.4 14.6a1 1 0 0 0-.57-.17H3.5V9.57h4.33a1 1 0 0 0 .57-.17l5.1-3.5z" />
+                </g>
+              </g>
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div className="captcha-row">
+        <input
+          ref={inputRef}
+          type="text"
+          value={inputValue}
+          onChange={(e) => inputOnChange(e)}
+          placeholder={placeholder}
+          className="captcha-input captcha-ui-input"
+        />
+      </div>
+      <div className="captcha-row">{validTips()}</div>
     </div>
   );
 };
