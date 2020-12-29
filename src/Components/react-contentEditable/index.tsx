bcb9954c24ef9d7b0a525514c835c6f47ee320b2
Kothing <39289309+kothing@users.noreply.github.com>
2020-12-29 14:58:40 +0800

    Create index.tsx

---
+++ src/Components/react-contentEditable/index.tsx
@@ -0,0 +1,47 @@
+import React, { useState, useRef, useEffect, useMemo } from 'react';
+import styles from './styles.less';
+
+interface EditingProps {
+  content?: string;
+  onChange?: (s: string) => void;
+}
+/**
+ * Editing
+ * @param { content, onChange }
+ */
+const Editing: React.FC<EditingProps> = ({ content, onChange }) => {
+  const [value, setValue] = useState<string>(content || '');
+  const editRef = useRef<HTMLDivElement>(null);
+
+  const onInput = () => {
+    const newValue = editRef.current?.innerText || '';
+    if (onChange) {
+      onChange(newValue);
+    }
+  };
+
+  useEffect(() => {
+    if (content === editRef.current?.innerText && content !== value) {
+      setValue(content || '');
+    }
+  }, [content, value]);
+
+  const edit = useMemo(
+    () => (
+      <div
+        ref={editRef}
+        className={styles.editing}
+        contentEditable="true"
+        suppressContentEditableWarning
+        onInput={onInput}
+      >
+        {value}
+      </div>
+    ),
+    [editRef],
+  );
+
+  return edit;
+};
+
+export default Editing;
